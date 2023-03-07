ae3e999a04fb991f07ce8285e73e7a3abbec71e3
vyas-git <vyasreddy.tech@gmail.com>
2023-03-07 20:34:35 +0530

    graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/vyas-git/go-microservices/handlers"
@@ -14,12 +16,25 @@ func main() {
 	hh := handlers.NewHello(l)
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
-	server := &http.Server{
+	s := &http.Server{
 		Addr:         ":9090",
 		Handler:      sm,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
-	server.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+	signChan := make(chan os.Signal)
+	signal.Notify(signChan, os.Interrupt)
+	signal.Notify(signChan, os.Kill)
+
+	sig := <-signChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
